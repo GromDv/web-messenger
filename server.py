fc12b1255d5edd9728aee0f18ebfd5a48acc64bc
GromDV <gromdv@gmail.com>
2023-10-19 12:58:43 +0300

    Initial commit

---
+++ server.py
@@ -0,0 +1,79 @@
+#!/bin/python3
+import socket
+import threading
+
+# Connection Data
+host = '127.0.0.1'
+port = 55005
+
+# Starting Server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+# Lists For Clients and Their Nicknames
+clients = []
+nicknames = []
+
+# Sending Messages To All Connected Clients
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def sendToNick(nickname, message):
+    #print("sending...")
+    for nick in nicknames:
+        if nick == nickname:
+            index = nicknames.index(nick)
+            clients[index].send(message)
+
+# Handling Messages From Clients
+def handle(client):
+    while True:
+        try:
+            # Broadcasting Messages
+            message = client.recv(1024)
+
+            mess = message.decode()
+            messTxt = mess[str(mess).find("~")+1:]
+            toNick = mess[0:str(mess).find("~")]
+            
+            if str(toNick).find("all") >= 0:
+                broadcast(messTxt.encode('ascii'))
+            else:
+                sendToNick(toNick, messTxt.encode('ascii'))
+
+        except:
+            # Removing And Closing Clients
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast('{} left!'.format(nickname).encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+# Receiving / Listening Function
+def receive():
+    while True:
+        # Accept Connection
+        client, address = server.accept()
+        print("Connected with {}".format(str(address)))
+
+        # Request And Store Nickname
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        # Print And Broadcast Nickname
+        print("Nickname is {}".format(nickname))
+        broadcast("{} joined!".format(nickname).encode('ascii'))
+        client.send('Connected to server!'.encode('ascii'))
+
+        # Start Handling Thread For Client
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+print("Server if listening...")
+receive()
